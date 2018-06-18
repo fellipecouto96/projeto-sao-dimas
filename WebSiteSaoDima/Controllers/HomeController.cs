3b9c535f4d60119a1c5b35e8d6756b9947793126
maximilion12 <matheus.maximo@sga.pucminas.br>
2018-06-18 01:20:58 -0300

    *

--- WebSiteSaoDima/Controllers/HomeController.cs
+++ WebSiteSaoDima/Controllers/HomeController.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebSiteSaoDima.Models;
 
 namespace WebSiteSaoDima.Controllers
 {
     public class HomeController : Controller
     {
+        Trab_LabEntities contexto = new Trab_LabEntities();
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult Login(string email, string senha)
+        {
+            string msg = "";
+            Usuario usu = contexto.Usuario.SingleOrDefault(u => u.email == email);
+
+            if(usu == null)
+            {
+                msg = "Usuário não existe.";
+            }
+            else if (usu.senha != senha)
+            {
+                msg = "Senha Incorreta.";
+            }
+            else
+            {
+                msg = "Bem vindo " + usu.nome + "!";
+                Session["Usuario"] = usu;
+            }
+            return View();
+        }
+        public ActionResult Cadastrar(Usuario usuario = null)
+        {
+
             return View();
         }
     }
